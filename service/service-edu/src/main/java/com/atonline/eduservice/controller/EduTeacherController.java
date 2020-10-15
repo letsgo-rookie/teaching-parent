34fde26a286bd658898e4001e5b06cd3325e1641
letsgo-rookie <zshw007@163.com>
2020-10-15 15:33:07 +0800

    Added pagination query and conditional pagination query for EduTeacher

--- service/service-edu/src/main/java/com/atonline/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/com/atonline/eduservice/controller/EduTeacherController.java
@@ -3,11 +3,15 @@ package com.atonline.eduservice.controller;
 
 import com.atonline.commonutils.R;
 import com.atonline.eduservice.entity.EduTeacher;
+import com.atonline.eduservice.entity.vo.TeacherQuery;
 import com.atonline.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,6 +34,8 @@ public class EduTeacherController {
     private EduTeacherService teacherService;
 
 
+    // 查询讲师表所有数据
+    //rest风格
     @ApiOperation(value = "所有讲师列表")
     @GetMapping("/findAll")
     public R list(){
@@ -37,11 +43,68 @@ public class EduTeacherController {
         return R.ok().data("items", list);
     }
 
+    // 逻辑删除讲师的方法
     @ApiOperation(value = "根据id逻辑删除讲师")
     @DeleteMapping("{id}")
     public R removeById(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id){
         teacherService.removeById(id);
         return R.ok();
+    }
+
+    //分页查询讲师的方法
+    //current 当前页
+    //limit 每页记录数
+    @ApiOperation(value = "讲师分页列表")
+    @GetMapping("{page}/{limit}")
+    public R pageList(@ApiParam(name = "page", value = "当前页码", required = true)
+                      @PathVariable Long current,
+                      @ApiParam(name = "limit", value = "每页记录数", required = true)
+                      @PathVariable Long limit){
+        Page<EduTeacher> pageParam = new Page<>(current, limit);
+        teacherService.page(pageParam, null);
+        List<EduTeacher> records = pageParam.getRecords();
+        long total = pageParam.getTotal();
+        return  R.ok().data("total", total).data("rows", records);
+    }
+
+    //条件查询带分页的方法
+    @ApiOperation(value = "根据条件分页查询")
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable Long current,
+                                  @PathVariable Long limit,
+                                  TeacherQuery teacherQuery) {
+        //创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+
+        //构建条件
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        // 多条件组合查询
+        // mybatis学过 动态sql
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        //判断条件值是否为空，如果不为空拼接条件
+        if(!StringUtils.isEmpty(name)) {
+            //构建条件
+            wrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)) {
+            wrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create",begin);
+        }
+        if(!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create",end);
+        }
+
+        //调用方法实现条件查询分页
+        teacherService.page(pageTeacher,wrapper);
+
+        long total = pageTeacher.getTotal();//总记录数
+        List<EduTeacher> records = pageTeacher.getRecords(); //数据list集合
+        return R.ok().data("total",total).data("rows",records);
     }
 
 }
